d5762acb1479eb0c16c534c94ec66dff32e62e08
majid <majidhajric@gmail.com>
2023-10-24 22:10:48 +0200

    create logout confirmation page

--- web-application/src/main/java/dev/majidhajric/authentication/web/DefaultErrorController.java
+++ web-application/src/main/java/dev/majidhajric/authentication/web/DefaultErrorController.java
@@ -3,15 +3,19 @@ package dev.majidhajric.authentication.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @ControllerAdvice
+@Controller
 public class DefaultErrorController implements ErrorController {
 
     @ExceptionHandler(Throwable.class)
+    @GetMapping("/error")
     public String exception(final Throwable throwable, final Model model) {
         log.error("Exception during execution of SpringSecurity application", throwable);
         String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
